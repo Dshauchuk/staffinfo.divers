7427794238d659687041a1aaff5e7c40f37a93eb
Dzmitry Shauchuk <dshauchuk@gmail.com>
2019-12-04 16:50:07 +0300

    Implemented getting of divers. Formatted the datatables, added default avatar for divers.

--- Controllers/DiversController.cs
+++ Controllers/DiversController.cs
@@ -1,16 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.AspNetCore.Mvc;
+using Staffinfo.Divers.Infrastructure.Attributes;
+using Staffinfo.Divers.Models;
+using Staffinfo.Divers.Services.Contracts;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 
 namespace staffinfo.divers.Controllers
 {
+    [JwtAuthorize]
     public class DiversController : Controller
     {
+        private readonly IDiverService _diverService;
+
+        public DiversController(IDiverService diverService)
+        {
+            _diverService = diverService;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public async Task<JsonResult> GetListJson([FromQuery]FilterOptions filter = null)
+        {
+            var divers = await _diverService.GetAsync(filter);
+
+            return Json(divers);
         }
     }
 }
