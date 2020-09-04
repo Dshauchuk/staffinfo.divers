e5ca3bd80f9d210d1a57d941ff52dc42d35d0065
Egor <e.danilkov@mail.ru>
2020-09-04 11:41:27 +0300

    Rename variable

--- src/wwwroot/js/globals.js
+++ src/wwwroot/js/globals.js
@@ -1,4 +1,4 @@
 ﻿// Storage space for global variables
 
 const ColorsEnum = Object.freeze({ "secondaryColor": "#CED4DA", "dangerColor": "#DC3545" });
-const AllowedExtension = [".jpg", ".png", ".jpeg", ".gif"];
+const AllowedAvatarFileExtensions = [".jpg", ".png", ".jpeg", ".gif"];
